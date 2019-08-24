caba6463a01ea17da24382c3579f57ebd0c595ce
Girish Vankudre <girish.vankudre@gmail.com>
2019-08-24 12:45:04 +0530

    Skewness function added to Measure_Centre

--- Measure_Centre.py
+++ Measure_Centre.py
@@ -5,3 +5,15 @@ def get_mean(list_1):
 def get_median(list_1):
     median1 = np.median(list_1)
     return (print ("Median of the dataset is: ",median1))
+
+def get_skewness(list_1):
+    SD1 = getSD(list_1)
+    mean2 = get_mean(list_1)
+    median2 = get_median(list_1)
+    skewness1 = (3*mean2*median2)/SD1
+    if skewness1 == 0:
+        return ("Dataset is Normal/Gaussian Distribution as skewness is ",skewness1)
+    if skewness1 > 0:
+        return ("Dataset is Left Skewed as skewness is ",skewness1)
+    if skewness1 < 0:
+        return ("Dataset is Right Skewed as skewness is ",skewness1)
