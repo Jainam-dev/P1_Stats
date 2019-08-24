772d166391b4e13dbbf5c15645e09eb1bc32d5d9
Girish Vankudre <girish.vankudre@gmail.com>
2019-08-24 12:27:43 +0530

    Measure_Centre added to calculate Mean and Median with function

---
+++ Measure_Centre.py
@@ -0,0 +1,7 @@
+def get_mean(list_1):
+    mean1 = np.mean(list_1)
+    return (print ("Mean of the dataset is: ",mean1))
+
+def get_median(list_1):
+    median1 = np.median(list_1)
+    return (print ("Median of the dataset is: ",median1))
